59bbb36acb4fe8178e52682e7dffcd2520629ebf
chawich <marc_chawich@hotmail.com>
2024-02-16 17:12:18 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+   <div className="headerTab">
+   <div className="headerContents">  
+        <button className="headerButton">/Home</button>
+        <button className="headerButton">/Expertise</button>
+        <button className="headerButton">/Work</button>
+        <button className="headerButton">/Experience</button>
+        <button className="headerButton">/Contact</button>
+   </div>
+   </div>
+   <div className="footertab">
+   <button className="footerbutton">linkdein</button>
+   <button className="footerbutton">whatsapp</button>
+   <button className="footerbutton">phone</button>
+   </div>
+   
+    
+
+
+</>
+  )
+}
+
+export default App;
